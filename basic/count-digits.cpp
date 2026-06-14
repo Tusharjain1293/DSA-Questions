b2f25cdbf49dd7b342036667ab8f0a334847fd5a
tushar <tusharjain1293@gmail.com>
2026-06-15 00:24:27 +0530

    count all digits in a number

---
+++ basic/count-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int countDigits(int n) {
+        // Code here
+        int count=0;
+        while(n>0)
+        {
+            n=n/10;
+            count++;
+        }
+        return count;
+        
+    }
+};
