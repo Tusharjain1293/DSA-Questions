a45596a9f122ed49bc192ce52bbae6b713f5b18d
tushar <tusharjain1293@gmail.com>
2026-06-14 13:26:36 +0530

    [200~Given an array arr, rotate the array by one position in clockwise direction.

---
+++ right_rotate_an_array_by_one_place.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        int temp=arr[arr.size()-1];
+        
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+        
+    }
+};
